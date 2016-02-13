3c724de075f2cc0aec6877fc74b3c065340d9d16
Sayan Chakraborty <mail.sayanc@gmail.com>
2016-02-13 18:54:53 +0530

    adfa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
   def bootstrap_class_for(flash_type)
     case flash_type
     when 'success'
-      'alert-success'
+      # 'alert-success'
     when 'error'
       'alert-danger'
     when 'warning'
