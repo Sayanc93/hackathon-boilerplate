9aa785d96cf86d4b0bb128f1877902b88bfe26e5
Sayan Chakraborty <mail.sayanc@gmail.com>
2016-02-13 19:41:33 +0530

    adfa

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
   def full_title(page_title = '')
-    # base_title = 'Hackathon'
-    # if page_title.empty?
-    #   base_title
-    # else
-    #   page_title + ' | ' + base_title
-    # end
+    base_title = 'Hackathon'
+    if page_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
   end
 
   def bootstrap_class_for(flash_type)
